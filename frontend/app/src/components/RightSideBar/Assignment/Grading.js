bd604196270bdd96019027670e84333a42c68527
Pratyush Duklan <pratu0906@gmail.com>
2022-04-11 13:06:48 -0400

    starter files for submission and grading

---
+++ frontend/app/src/components/RightSideBar/Assignment/Grading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { AnimatePresence } from 'framer-motion'
+
+const Grading = () => {
+  return (
+      <>
+          
+          
+      </>
+  )
+}
+
+export default Grading
